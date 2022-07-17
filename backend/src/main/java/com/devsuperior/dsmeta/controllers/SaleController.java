c0b84d27cccda3f77770255afd1ff7717393de2e
tiagohlazarinidev <tiago_lazarini@hotmail.com>
2022-07-17 12:52:06 -0300

    Endpoint notification

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -2,19 +2,18 @@ package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
     @Autowired
     private SaleService service;
+    private SmsService smsService;
 
     /* http://localhost:8080/sales?minDate=2022-01-01&maxDate=2022-03-31*/
 
@@ -24,5 +23,10 @@ public class SaleController {
             @RequestParam(value="maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
